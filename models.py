fc13ec5c0c5abe43ecfff5442e08d4381a0a2336
Michael Lee <michaellee1019@gmail.com>
2024-07-10 14:50:18 -0400

    test namespace changing

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ import sys
 import smbus2
 
 class i2cdetect(Sensor):
-    MODEL = "michaellee1019:i2cdetect:i2cdetect"
+    MODEL = "viam-dev-staging:i2cdetect:i2cdetect"
     i2c_bus = None
 
     @classmethod
