baf0051f6345c4b23dcf5a880551ac9d05dde07a
Michael Lee <michaellee1019@gmail.com>
2025-06-04 12:50:57 -0400

    test module rename

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ import sys
 import smbus2
 
 class i2cdetect(Sensor):
-    MODEL = "michaellee1019:i2cdetect:i2cdetect"
+    MODEL = "michaellee1019-2:i2cdetect-renamed:i2cdetect"
     i2c_bus: int = 1
 
     @classmethod
